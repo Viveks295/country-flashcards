58bcb92eca2294a9f2f4e26d7405d007dc3097e1
Viveks295 <viveksaravanan295@gmail.com>
2022-08-24 12:54:37 -0500

    deployment

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+from audioop import cross
+from flask import (Flask, abort, jsonify)
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder='../react-frontend/build', static_url_path='')
+CORS(app)
+
 
 from model import db, save_db
 
-@app.route("/")
-def welcome():
-   return jsonify(db)
-
 
 @app.route("/cards")
+@cross_origin()
 def api_card_list():
   return jsonify(db)
-  abort(404)
+
+@app.route('/')
+@cross_origin
+def serve():
+  return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == "__main__":
   app.run(host='127.0.0.1', port=5001)
